37c63333ed76de7e5198d0fe15ed24a61099f683
Kesinee Powers <kesineepowers@Kesinees-MacBook-Pro.local>
2024-01-11 11:19:03 -0500

    create a class Account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import account.Account;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Account account = new Account(100, 0.025);
+        account.status();
+        account.deposit(20);
+        account.withdraw(45.50);
+        account.withdraw(200);
+        account.status();
     }
 }
---
+++ src/account/Account.java
@@ -0,0 +1,35 @@
+package account;
+
+public class Account {
+    private double balance;
+    private double interestRate;
+
+    public Account(double balance, double interestRate) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public boolean withdraw(double amount) {
+        if(balance < amount) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void status() {
+        System.out.printf("Balance: %.2f\n", balance);
+    }
+}
